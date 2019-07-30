ea1fa131e5788819045892c6793418406721ffe3
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:26:14 -0400

    flask intro

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+    # return "mlookhieh"
+	food_fav = ["mlookhieh","upside down","pizza"]
+	food_no = ["olives", "beans", "liver"]
+	return render_template("index.html" ,food_fav=food_fav, food_no=food_no, opposite_day=opposite_day)
 
+opposite_day = False;
 if __name__ == '__main__':
    app.run(debug = True)
